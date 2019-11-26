0c9676ee8304605ade66741d18b3d77e445d2ed4
kiransainath2163 <kiransainath2163@gmail.com>
2019-11-26 09:53:13 +0530

    Indentation

--- NodeJsUdemy/notes-app/app.js
+++ NodeJsUdemy/notes-app/app.js
@@ -3,46 +3,46 @@ const yargs = require('yargs')
 yargs.version('1.1.0')
 
 yargs.command({
-    command:'add',
-    describe:'Adding a new note',
-    builder:{
-        title:{
-             describe:'Note Title',
-             demandOption:true,
-             type:'string'
+    command: 'add',
+    describe: 'Adding a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
         },
-        body:{
-            describe:'Body Content',
-            demandOption:true,
-            type:'string'
+        body: {
+            describe: 'Body Content',
+            demandOption: true,
+            type: 'string'
         }
-        
+
     },
-    handler:function(argv){
-        console.log('Adding a new note'+argv.title+' '+argv.body)
+    handler: function (argv) {
+        console.log('Adding a new note' + argv.title + ' ' + argv.body)
     }
 })
 
 yargs.command({
-    command:'remove',
-    describe:'HRemoving a note',
-    handler:function(){
+    command: 'remove',
+    describe: 'HRemoving a note',
+    handler: function () {
         console.log('Removing a note')
     }
 })
 
 yargs.command({
-    command:'list',
-    describe:'Listing notes',
-    handler:function(){
+    command: 'list',
+    describe: 'Listing notes',
+    handler: function () {
         console.log('Listing notes')
     }
 })
 
 yargs.command({
-    command:'read',
-    describe:'Reading notes',
-    handler:function(){
+    command: 'read',
+    describe: 'Reading notes',
+    handler: function () {
         console.log('Reading notes')
     }
 })
