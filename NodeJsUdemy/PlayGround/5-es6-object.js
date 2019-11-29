f20501f8c410675f9a9d0358c21aaaaa50bbe45a
kiransainath2163 <kiransainath2163@gmail.com>
2019-11-29 10:54:25 +0530

    Section 6.39

---
+++ NodeJsUdemy/PlayGround/5-es6-object.js
@@ -0,0 +1,24 @@
+const name = 'Kiran'
+const userAge = 27
+
+const user = {
+    name: name,
+    age: userAge,
+    location: 'Philadelphia'
+}
+
+console.log(user)
+
+//Destructuring an object
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating:undefined
+}
+
+let { label, stock, salePrice='gafafsa', rating = 45 } = product
+
+console.log(label, stock, salePrice, rating)
